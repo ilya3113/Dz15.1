988e43e0e73863f113b2fa8767c2b428a5997484
ilyak <ilya.kep.10@mail.ru>
2024-02-25 14:22:37 +0300

    dz

--- scr/main.py
+++ scr/main.py
@@ -1,17 +1,16 @@
-from src.category import Category
-from src.product import Product
-from src.product_iterator import ProductIterator
-from utils.pars_json import creates_instance_class
+from scr.category import Category
+from scr.product import Product
+from test.pars_json import creates_instance_class
 
 
 def main():
     instance_category, instance_product = creates_instance_class()
     cat = Category(*instance_category[0])
-    list_ = cat.product
-    pi = ProductIterator(list_)
-    for i in pi:
-        print(i)
-
-
-if __name__ == "__main__":
-    main()
+    cat.add_product(Product.creates_product({
+        "name": "Xiaomi 14 Pro",
+        "description": "Влагозащищенный корпус",
+        "price": 190_000.0,
+        "quantity": 5
+    }))
+    print(cat.product)
+    # Smartphone("Samsung Galaxy", "Хорошая камера", 180_000.0, 5, 2.5, "C23 Ultra", 6, "Серый цвет")
