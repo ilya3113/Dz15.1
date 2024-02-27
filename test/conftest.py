4acf64b8fa8c5673b02e5f3bf9e9b28bd483ad60
Kapatsin Ilya <70647559+ilya3113@users.noreply.github.com>
2024-02-27 15:41:30 +0300

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,4 @@
 import pytest
-from scr.category import Category
-from scr.product import Product
 from scr.product_iterator import ProductIterator
 from pars_json import creates_instance_class
 from scr.lawn_grass import LawnGrass
@@ -10,15 +8,13 @@ from scr.smartphone import Smartphone
 @pytest.fixture
 def test_category():
     instance_category, instance_product = creates_instance_class()
-    for category in instance_category:
-        return Category(*category)
+    return instance_category[0]
 
 
 @pytest.fixture
 def test_product():
     instance_category, instance_product = creates_instance_class()
-    for product in instance_product:
-        return Product(*product)
+    return instance_product[0]
 
 
 @pytest.fixture
@@ -29,7 +25,8 @@ def test_pars_json():
 @pytest.fixture
 def test_iterator():
     instance_category, instance_product = creates_instance_class()
-    return ProductIterator(instance_category[0][2])
+    return ProductIterator(instance_category[0].product)
+
 
 @pytest.fixture
 def test_smartphone():
